54a2df87eebddaf40ad852247703460253613b3e
Ali Mihandoost <i@ali.md>
2016-05-21 01:35:05 +0430

    feat: addurl api

--- src/index.js
+++ src/index.js
@@ -34,20 +34,27 @@ makeServer = () => {
 
 serverListener = (req, res) => {
   log(`New request: ${req.url}`);
-  req.url = URL.parse(req.url);
+  req.url = URL.parse(req.url, true);
 
   if (!checkInternalRouters(req, res)) {
-    redirect();
+    redirect(req, res);
   }
 
   res.end();
 },
 
 checkInternalRouters = (req, res) => {
+  log('checkInternalRouters');
   let ret = true;
-  switch (req.url.path) {
+  switch (req.url.pathname) {
     case config.not_found:
-      page404();
+      page404(req, res);
+      break;
+
+    case '/addurl':
+      addurl(req, res);
+      break;
+
     default:
       ret = false;
   }
@@ -55,13 +62,28 @@ checkInternalRouters = (req, res) => {
 },
 
 page404 = (req, res) => {
+  log('page404');
   res.writeHead(404, {
     'Content-Type': 'text/html'
   });
-  res.write('<h1 style="text-align: \'center\'; margin-top: 2em;">Not found!</h1>');
+  res.write('<h1 style="text-align: center; margin-top: 1em; font-size: 7em;">404</h1>');
+},
+
+addurl = (req, res) => {
+  log('addurl');
+  log(req.url.query);
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  shortener.addurl(req.url.query.short, req.url.query.url);
+  res.write(`<p style="text-align: center; margin-top: 1em; font-size: 1.2em;">
+    Success.<br/>
+    <a href="/${req.url.query.short}">ali.md/${req.url.query.short}</a> =&gt; ${req.url.query.url}
+  </p>`);
 },
 
 redirect = (req, res) => {
+  log('redirect');
   let expanded = shortener.find(req.url.pathname) || {url: config.not_found};
   res.writeHead(expanded.mode === 'permanently' ? 301 : 302, {
     Location: expanded.url
--- src/shortener.js
+++ src/shortener.js
@@ -14,14 +14,20 @@ export function cleanUrl (path) {
   if (path.indexOf('/') === 0) path = path.substr(1);
   if (path.lastIndexOf('/') === path.length-1) path = path.substr(0, path.length-1);
   if (path.trim().length === 0) path = '/';
-  log(`${url} => ${path}`);
   return path;
 }
 
 export function find (url) {
   log(`find url`);
-  url = cleanUrl(url);
-  let item = db.query(url);
+  let curl = cleanUrl(url);
+  log(`clean url ${url} => ${curl}`);
+  let item = db.query(curl);
   if (typeof item === 'string') item = {url: item};
   return item || false
+}
+
+export function addurl(short, url) {
+  log(`addurl ${short} => ${url}`);
+  short = cleanUrl(short);
+  db.insert(short, url);
 }
