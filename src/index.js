c7de28d46413a20413a63094980f18ab7f308e29
Ali Mihandoost <i@ali.md>
2016-05-21 11:18:12 +0430

    feat(index): improve templates

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ log = debug('ali-dot-md-slash:server'),
 config = {
   host: process.env.alimd_host || '0.0.0.0',
   port: process.env.alimd_port || '8080',
-  not_found: process.env.alimd_notfound || '/404/'
+  not_found: process.env.alimd_notfound || '/404/',
+  addurl: '/addurl'
 },
 
 main = () => {
@@ -50,7 +51,7 @@ checkInternalRouters = (req, res) => {
       page404(req, res);
       break;
 
-    case '/addurl':
+    case config.addurl:
       addurl(req, res);
       break;
 
@@ -65,7 +66,10 @@ page404 = (req, res) => {
   res.writeHead(404, {
     'Content-Type': 'text/html'
   });
-  res.write('<h1 style="text-align: center; margin-top: 1em; font-size: 7em;">404</h1>');
+  res.write(`<!DOCTYPE html><html><body>
+  <h1 style="text-align: center; margin-top: 1em; font-size: 7em;">404</h1>
+  <p style="text-align: center; margin-top: 1.5em; font-size: 1.2em;">If you want to add this just tell me <a href="https://github.com/AliMD/ali-dot-md-slash/issues/new">here</a></p>
+  </body></html>`);
 },
 
 addurl = (req, res) => {
@@ -74,11 +78,23 @@ addurl = (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  shortener.addurl(req.url.query.short, req.url.query.url);
-  res.write(`<p style="text-align: center; margin-top: 1em; font-size: 1.2em;">
-    Success.<br/>
-    <a href="/${req.url.query.short}">ali.md/${req.url.query.short}</a> =&gt; ${req.url.query.url}
-  </p>`);
+
+  if (shortener.addurl(req.url.query.short || '', req.url.query.url || '')) {
+    res.write(`<!DOCTYPE html><html><body>
+    <p style="text-align: center; margin-top: 1em; font-size: 1.2em;">
+      Success.<br/>
+      <a href="/${req.url.query.short}">ali.md/${req.url.query.short}</a> =&gt; ${req.url.query.url}
+    </p>
+    </body></html>`);
+  } else {
+    res.write(`<!DOCTYPE html><html><body>
+    <form action="http://go.ali.md${config.addurl}" target="_blank">
+      <input type="text" name="short" value="" />
+      <input type="text" name="url" value="http://" />
+      <input type="submit" value="Send" />
+    </form>
+    </body></html>`);
+  }
 },
 
 redirect = (req, res) => {
